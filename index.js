db7f25dd5197f5872b6f284919477d98212542e5
Janet Mendez <janetmendez.03@gmail.com>
2019-09-05 16:25:29 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tut) => {
+    return tut.split(' ').map((word) => {
+        return word.charAt(0).toUpperCase() + word.substr(1)
+    }).join(' ')
+  })
 }
